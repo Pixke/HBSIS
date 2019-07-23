b3fa1b31fb93c11678e1ba71ac35d794d156fac3
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-23 15:07:27 -0300

    Final QueryInnerJoin

---
+++ QuerysInnerJoin/SQLQuery2.sql
@@ -0,0 +1,8 @@
+﻿select
+sum (Valor * Quantidade) as 'Valor Total', CONCAT(month (DatInc),'/', YEAR (DatInc)) as 'Mês/Ano'
+from Vendas 
+group by CONCAT(month (DatInc),'/', YEAR (DatInc)) 
+order by [Valor Total] desc
+
+--Trazer o mês de cada ano que retornou a maior quantidade de vendas
+-- Tradução "Retornar agrupado por mês a quantidade de vendas e ordernar do maior para menor"
