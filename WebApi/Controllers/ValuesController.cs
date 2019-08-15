811a9bd5b413af3724caacf22326c273fb2053eb
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-15 17:28:52 -0300

    wFEFGF

---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ValuesController : ApiController
+    {
+        LocacaoDB LocacaoDB = new LocacaoDB();
+        // GET api/values
+        public List<Carro> GET()
+        {
+            return LocacaoDB.Carros.ToList<Carro>();
+        } // GET api/values/5
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
